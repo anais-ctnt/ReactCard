9cdfaa4429dbaf8fae803fd3e9eef382d2a05bd0
anais-ctnt <anaiscotinet@gmail.com>
2020-10-27 13:37:27 +0100

    add react card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const sampleCard = {
+  title: "Delectus eaque",
+  description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius magnam animi pariatur libero tempora esse neque saepe explicabo at incidunt. Quaerat !",
+  image: "https://i.imgur.com/wmigLDH.png",
+  favorite: false,
+}
+
+const App = () => {
+ 
+  return(
+    <div className="cardContainer">
+      <img className="cardImg"
+      src={sampleCard.image}
+      alt={sampleCard.title}
+        />
+      <h2 className="cardTitle">
+        {sampleCard.title}
+      </h2>
+      <p className="cardDescription">{sampleCard.description}</p>
+      <div className="cardIcons">
+        <div className="cardIcon">
+          <div 
+          className="fav">&#9733;
+          </div>
+          <p>FAVORITE</p>
+        </div>
+        <div className="cardIcon">
+          <div className="share">&#10150;</div> 
+          <p>SHARE</p>
+        </div>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
